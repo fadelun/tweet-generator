4134da59da0bbc8ac4ea14e9d1c13ec906d4c7b3
Muhammad Fadll <fadeljourney@gmail.com>
2023-04-01 14:05:15 +0700

    add moment.js

--- src/components/TheDate.vue
+++ src/components/TheDate.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="twitter-date w-full">
     <p class="text-base font-medium">
-      <span class="text-slate-400">12:30 PM · Apr 21, 2021 ·</span>
+      <span class="text-slate-400">{{ timestanmp }}</span>
       <span class="text-blue-400"> Twitter Web App</span>
     </p>
   </div>
 </template>
-<script></script>
+<script setup>
+import { computed } from "vue";
+
+import moment from "moment";
+
+const date = new Date();
+const timestanmp = computed(() => {
+  return moment(date).format("h:mm A · MMM DD, YYYY ·");
+});
+</script>
